20b7e726e9f92363531ef2228eca46c62d06c843
belanovic <goranbelanovic@yahoo.com>
2024-08-07 00:13:23 +0200

    msg

--- src/handleImageDB.js
+++ src/handleImageDB.js
@@ -1,9 +1,17 @@
 import firebase from './firebase.js';
 const storage = firebase.storage();
 
+let imagesFolder;
+if(process.env.REACT_APP_COPY == '0') {
+    imagesFolder = 'site-news-images'
+} else {
+    imagesFolder = `site-news-images${process.env.REACT_APP_COPY}`;
+}
+
+
 export async function uploadImageDB(imgName, imgFile, folder) {
     try {
-        const ref8 = storage.ref(`site-news-images/${folder}` + imgName);
+        const ref8 = storage.ref(`${imagesFolder}/${folder}` + imgName);
         const snapshot = await ref8.put(imgFile);
         const photoURL = await ref8.getDownloadURL();
         return photoURL
@@ -16,7 +24,7 @@ export async function uploadImageDB(imgName, imgFile, folder) {
 export async function removeImageDB(imgName, folder) {
 
     try {
-        const imgRef = await storage.ref(`site-news-images/${folder}` + imgName);
+        const imgRef = await storage.ref(`${imagesFolder}/${folder}` + imgName);
         const promiseResolve = await imgRef.delete();
         return 'image deleted'
     } catch (error) {
--- src/handleVideoDB.js
+++ src/handleVideoDB.js
@@ -2,9 +2,17 @@ import firebase from './firebase.js';
 
 const storage = firebase.storage();
 
+let imagesFolder;
+
+if(process.env.REACT_APP_COPY == '0') {
+    imagesFolder = 'site-news-images'
+} else {
+    imagesFolder = `site-news-images${process.env.REACT_APP_COPY}`;
+}
+
 export async function uploadVideoDB (videoName, videoFile) {
     try {
-        const ref8 = storage.ref('site-news-images/' + videoName);
+        const ref8 = storage.ref(`${imagesFolder}/` + videoName);
         const snapshot = await ref8.put(videoFile);
         const videoURL = await ref8.getDownloadURL();
         return videoURL
@@ -15,7 +23,7 @@ export async function uploadVideoDB (videoName, videoFile) {
 }
 export async function removeVideoDB(videoName) {
     try {
-        const videoRef = await storage.ref('site-news-images/' + videoName);
+        const videoRef = await storage.ref(`${imagesFolder}/` + videoName);
         const promiseResolve = await videoRef.delete();
         return 'Videoe deleted'
     } catch (error) {
