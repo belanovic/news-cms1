ae0cb16299f79ff2da00126f76b7cb0c6a5c3dde
belanovic <goranbelanovic@yahoo.com>
2024-08-06 23:02:13 +0200

    msg

--- src/hostBackend.js
+++ src/hostBackend.js
@@ -1,4 +1,8 @@
+let link;
 
-const HOST_BACKEND = process.env.NODE_ENV == 'production'? 'https://linux-news-backend.onrender.com' : 'http://localhost:4000'
+if(process.env.REACT_APP_COPY == '0') link = `https://linux-news-backend.onrender.com`;
+if(process.env.REACT_APP_COPY == '1') link = `https://news-backend1-jazp.onrender.com`;
+
+const HOST_BACKEND = process.env.NODE_ENV == 'production'? link : 'http://localhost:4000'
 
 export default HOST_BACKEND;
