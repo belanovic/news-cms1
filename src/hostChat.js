b3e454350693d9a51cdf37f4efa17219d3d47823
belanovic <goranbelanovic@yahoo.com>
2024-08-12 13:21:18 +0200

    msg

--- src/hostChat.js
+++ src/hostChat.js
@@ -1,7 +1,7 @@
 let link;
 
 if(process.env.REACT_APP_COPY == '0') link = `https://linux-news-chat.onrender.com`;
-if(process.env.REACT_APP_COPY == '1') link = `https://news-chat1.onrender.com`;
+if(process.env.REACT_APP_COPY != '0') link = `https://news-chat1.onrender.com`;
 
 const HOST_CHAT = process.env.NODE_ENV == 'production'? link : 'http://localhost:4001'
 
